9b535f011474df9a7e7fcdaedad7f748d3e964b8
sean@smgoodrich.com <sean@smgoodrich.com>
2024-12-02 17:42:52 -0800

    Map unique color from glb entity to three.js mesh. Add lil-gui with display options for pockets and unique color.

--- app/src/model/model.tsx
+++ app/src/model/model.tsx
@@ -5,14 +5,47 @@ import * as THREE from 'three';
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three-stdlib';
+import GUI from 'lil-gui';
+import rgbIdToEntityIdMap from "../../../data_dump/rgb_id_to_entity_id_map.json";
 
 interface ModelEntity {
     bufferGeometry: THREE.BufferGeometry;
-    color: string;
+    color: THREE.Color;
+    entityId: string;
+}
+
+// GUI settings for the Three.js scene.
+interface Settings {
+    mode : displayMode;
+};
+
+// Display modes for viewing mesh.
+enum displayMode {
+    pocket = "Pocket",
+    colorMap = "Color Map",
+}
+
+// Convert Color to dash seperated string EG: 255-0-100.
+function getRgbDashString(color: THREE.Color): string {
+    return Math.floor(255 * color.r) + '-'
+        + Math.floor(255 * color.g)  + '-'
+        + Math.floor(255 * color.b) ;
+}
+
+// Get entity's color based on display mode.
+function getColor(settings: Settings, entity: ModelEntity): string {
+    if (settings.mode == displayMode.colorMap) {
+        return entity.color.getStyle();
+    }
+
+    return 'rgb(100, 100, 100)';
 }
 
 export const Model = (): JSX.Element => {
     const [modelEnts, setModelEnts] = React.useState<ModelEntity[]>([]);
+    const [gui, setGui] = React.useState<GUI | null>(null);
+    const [settings, setSettings] = React.useState<Settings | null>(null);
+    const inputRef = React.useRef(null);
 
     React.useEffect(() => {
         new GLTFLoader().load('./colored_glb.glb', gltf => {
@@ -21,20 +54,40 @@ export const Model = (): JSX.Element => {
                 if (element.type !== 'Mesh') return;
 
                 const meshElement = element as THREE.Mesh;
+                const material = meshElement.material as THREE.MeshStandardMaterial;
+                const color = material.color as THREE.Color;
+                const entityId = rgbIdToEntityIdMap[getRgbDashString(color)];
                 newModuleEntities.push({
                     bufferGeometry: meshElement.geometry as THREE.BufferGeometry,
-                    color: 'rgb(120, 120, 120)',
+                    color: color,
+                    entityId: entityId,
                 });
             });
             setModelEnts(newModuleEntities);
         });
 
+        const gui = new GUI( { container: inputRef.current } );
+        const settings : Settings = {
+            mode: displayMode.pocket,
+        };
+        setSettings(settings);
+        gui.add(settings, 'mode', [displayMode.pocket, displayMode.colorMap])
+          	.name('Display mode')
+          	.onChange(newValue => {
+                setSettings(prevSettings => ({ ...prevSettings, mode: newValue}));
+          	});
+        setGui(gui);
+
+        return () => {
+            gui.destroy();
+        };
     }, [])
 
     return (
-        <div className="canvas-container">
+        <div className="canvas-container" ref={inputRef}>
             <Canvas camera={{ position: [0, 0, 300] }} >
                 <ambientLight />
+                <pointLight intensity={0.5} position={[0, 100, 0]} />
                 <OrbitControls makeDefault />
                 <group>
                     {
@@ -43,7 +96,7 @@ export const Model = (): JSX.Element => {
                                 geometry={ent.bufferGeometry}
                                 key={index}
                             >
-                                <meshStandardMaterial color={ent.color} />
+                                <meshStandardMaterial color={getColor(settings, ent)} />
                             </mesh>
                         ))
                     }
