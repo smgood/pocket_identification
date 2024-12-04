ce9c74b99476b6c0c0db70134ce2e86728116fc3
sean@smgoodrich.com <sean@smgoodrich.com>
2024-12-03 17:18:30 -0800

    fix inconsistent spacing in pocket detector class

--- app/src/pocket_detector.tsx
+++ app/src/pocket_detector.tsx
@@ -3,60 +3,60 @@ import adjacencyGraphEdgeMetadata from '../../data_dump/adjacency_graph_edge_met
 import entityGeometryInfo from '../../data_dump/entity_geometry_info.json';
 
 export default class PocketDetector {
-  private entityIds: string[];
-  private concaveEntities: Set<string>;
-  private pocketCount: number;
+    private entityIds: string[];
+    private concaveEntities: Set<string>;
+    private pocketCount: number;
 
-  constructor() {
-      this.isEntityPartOfPocket = this.isEntityPartOfPocket.bind(this);
+    constructor() {
+        this.isEntityPartOfPocket = this.isEntityPartOfPocket.bind(this);
 
-      this.entityIds = entityGeometryInfo.map(entity => entity["entityId"]);
-      this.concaveEntities = new Set<string>(this.entityIds);
-      const edges = Object.keys(adjacencyGraphEdgeMetadata);
+        this.entityIds = entityGeometryInfo.map(entity => entity["entityId"]);
+        this.concaveEntities = new Set<string>(this.entityIds);
+        const edges = Object.keys(adjacencyGraphEdgeMetadata);
 
-      // Remove all entities that share a convex edge.
-      for (var i = 0; i < edges.length; i++) {
-          if (adjacencyGraphEdgeMetadata[edges[i]].includes(1)) {
-              const newEntities = edges[i].split('-');
-              this.concaveEntities.delete(newEntities[0]);
-              this.concaveEntities.delete(newEntities[1]);
-          }
-      }
+        // Remove all entities that share a convex edge.
+        for (var i = 0; i < edges.length; i++) {
+            if (adjacencyGraphEdgeMetadata[edges[i]].includes(1)) {
+                const newEntities = edges[i].split('-');
+                this.concaveEntities.delete(newEntities[0]);
+                this.concaveEntities.delete(newEntities[1]);
+            }
+        }
 
-      this.pocketCount = this.determinePocketCount();
-  }
+        this.pocketCount = this.determinePocketCount();
+    }
 
-  private determinePocketCount(): number {
-    const concaveEntitiesList = Array.from(this.concaveEntities);
-    const unvisitedConcaveEntities = new Set([...concaveEntitiesList]);
-    var pocketCount = 0;
+    private determinePocketCount(): number {
+        const concaveEntitiesList = Array.from(this.concaveEntities);
+        const unvisitedConcaveEntities = new Set([...concaveEntitiesList]);
+        var pocketCount = 0;
 
-    for (var i = 0; i < concaveEntitiesList.length; i++) {
-        const entityId = concaveEntitiesList[i];
-        if (unvisitedConcaveEntities.has(entityId)){
-            pocketCount++;
-            this.checkNeighbors(entityId, unvisitedConcaveEntities);
+        for (var i = 0; i < concaveEntitiesList.length; i++) {
+            const entityId = concaveEntitiesList[i];
+            if (unvisitedConcaveEntities.has(entityId)){
+                pocketCount++;
+                this.checkNeighbors(entityId, unvisitedConcaveEntities);
+            }
+        }
+
+        return pocketCount;
+    }
+
+    private checkNeighbors(currentEntityId, unvisitedConcaveEntities) {
+        const neighbors = adjacencyGraph[currentEntityId];
+        for (var i = 0; i < neighbors.length; i++) {
+            const neighborId = neighbors[i];
+            if (unvisitedConcaveEntities.has(neighborId)) {
+                unvisitedConcaveEntities.delete(neighborId);
+                this.checkNeighbors(neighborId, unvisitedConcaveEntities);
+            }
         }
     }
 
-    return pocketCount;
-  }
+    // Returns whether an entity is part of a pocket.
+    public isEntityPartOfPocket = (entityId: string) =>
+        this.concaveEntities.has(entityId);
 
-  private checkNeighbors(currentEntityId, unvisitedConcaveEntities) {
-      const neighbors = adjacencyGraph[currentEntityId];
-      for (var i = 0; i < neighbors.length; i++) {
-          const neighborId = neighbors[i];
-          if (unvisitedConcaveEntities.has(neighborId)) {
-              unvisitedConcaveEntities.delete(neighborId);
-              this.checkNeighbors(neighborId, unvisitedConcaveEntities);
-          }
-      }
-  }
-
-  // Returns whether an entity is part of a pocket.
-  public isEntityPartOfPocket = (entityId: string) =>
-      this.concaveEntities.has(entityId);
-
-  // Returns the total number of pockets in the 3d object.
-  public getPocketCount = () => this.pocketCount;
+    // Returns the total number of pockets in the 3d object.
+    public getPocketCount = () => this.pocketCount;
 }
