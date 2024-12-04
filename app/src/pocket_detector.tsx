9693cba9db7e1047423af2db8cbeadf4eeac01d0
sean@smgoodrich.com <sean@smgoodrich.com>
2024-12-03 16:49:12 -0800

    Change misnamed variables from convex entities to concave entities

--- app/src/pocket_detector.tsx
+++ app/src/pocket_detector.tsx
@@ -4,22 +4,22 @@ import entityGeometryInfo from '../../data_dump/entity_geometry_info.json';
 
 export default class PocketDetector {
   entityIds: string[];
-  convexEntities: Set<string>;
+  concaveEntities: Set<string>;
   pocketCount: number;
 
   constructor() {
       this.isEntityPartOfPocket = this.isEntityPartOfPocket.bind(this);
 
       this.entityIds = entityGeometryInfo.map(entity => entity["entityId"]);
-      this.convexEntities = new Set<string>(this.entityIds);
+      this.concaveEntities = new Set<string>(this.entityIds);
       const edges = Object.keys(adjacencyGraphEdgeMetadata);
 
-      // Remove all Convex entities
+      // Remove all entities that share a convex edge.
       for (var i = 0; i < edges.length; i++) {
           if (adjacencyGraphEdgeMetadata[edges[i]].includes(1)) {
               const newEntities = edges[i].split('-');
-              this.convexEntities.delete(newEntities[0]);
-              this.convexEntities.delete(newEntities[1]);
+              this.concaveEntities.delete(newEntities[0]);
+              this.concaveEntities.delete(newEntities[1]);
           }
       }
 
@@ -27,34 +27,34 @@ export default class PocketDetector {
   }
 
   determinePocketCount(): number {
-    const convexEntitiesList = Array.from(this.convexEntities);
-    const unvisitedConvexEntities = new Set([...convexEntitiesList]);
+    const concaveEntitiesList = Array.from(this.concaveEntities);
+    const unvisitedConcaveEntities = new Set([...concaveEntitiesList]);
     var pocketCount = 0;
 
-    for (var i = 0; i < convexEntitiesList.length; i++) {
-        const entityId = convexEntitiesList[i];
-        if (unvisitedConvexEntities.has(entityId)){
+    for (var i = 0; i < concaveEntitiesList.length; i++) {
+        const entityId = concaveEntitiesList[i];
+        if (unvisitedConcaveEntities.has(entityId)){
             pocketCount++;
-            this.checkNeighbors(entityId, unvisitedConvexEntities);
+            this.checkNeighbors(entityId, unvisitedConcaveEntities);
         }
     }
 
     return pocketCount;
   }
 
-  checkNeighbors(currentEntityId, unvisitedConvexEntities) {
+  checkNeighbors(currentEntityId, unvisitedConcaveEntities) {
       const neighbors = adjacencyGraph[currentEntityId];
       for (var i = 0; i < neighbors.length; i++) {
           const neighborId = neighbors[i];
-          if (unvisitedConvexEntities.has(neighborId)) {
-              unvisitedConvexEntities.delete(neighborId);
-              this.checkNeighbors(neighborId, unvisitedConvexEntities);
+          if (unvisitedConcaveEntities.has(neighborId)) {
+              unvisitedConcaveEntities.delete(neighborId);
+              this.checkNeighbors(neighborId, unvisitedConcaveEntities);
           }
       }
   }
 
   isEntityPartOfPocket(entityId: string): boolean {
-      return this.convexEntities.has(entityId);
+      return this.concaveEntities.has(entityId);
   }
 
   getPocketCount(): number {
